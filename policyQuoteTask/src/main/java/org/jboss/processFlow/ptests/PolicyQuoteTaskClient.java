bdb7a9241cbe408c39b38f25edab9d33dd0865f0
Jeffrey Bride <jbride@zareason.com>
2013-10-20 08:31:54 -0600

    renamving simpleTask to policyQuoteTask

--- policyQuoteTask/src/main/java/org/jboss/processFlow/ptests/PolicyQuoteTaskClient.java
+++ policyQuoteTask/src/main/java/org/jboss/processFlow/ptests/PolicyQuoteTaskClient.java
@@ -17,6 +17,9 @@ import org.apache.jmeter.samplers.SampleResult;
 
 import org.apache.log.Logger;
 
+import org.kie.api.task.model.Status;
+import org.kie.api.task.model.TaskSummary;
+
 import org.jboss.processFlow.knowledgeService.IKnowledgeSession;
 import org.jboss.processFlow.tasks.ITaskService;
 import org.jboss.processFlow.test.EJBServicesFactory;
@@ -24,10 +27,11 @@ import org.jboss.processFlow.test.EJBServicesFactory;
 import org.acme.insurance.Policy;
 import org.acme.insurance.Driver;
 
-public class SimpleTaskClient extends AbstractJavaSamplerClient {
+public class PolicyQuoteTaskClient extends AbstractJavaSamplerClient {
 
     private static final String DEPLOYMENT_ID = "deploymentId";
     private static final String PROCESS_ID = "processId";
+    private static final String USER_ID = "userId";
 
     private Logger log = getLogger();
 
@@ -35,13 +39,15 @@ public class SimpleTaskClient extends AbstractJavaSamplerClient {
     private String processId;
     private boolean claimTask = true;
     private boolean completeTask = true;
+    private String userId;
     
     public SampleResult runTest(JavaSamplerContext context) {
-        ITaskService taskServiceProxy = EJBServicesFactory.getTaskServiceProxy();
-        IKnowledgeSession kSessionProxy = EJBServicesFactory.getKnowledgeSessionProxy();
+        ITaskService tProxy = EJBServicesFactory.getTaskServiceProxy();
+        IKnowledgeSession kProxy = EJBServicesFactory.getKnowledgeSessionProxy();
 
         deploymentId = System.getProperty(DEPLOYMENT_ID, "git-playground");
-        processId = System.getProperty(PROCESS_ID, "simpleTask");
+        processId = System.getProperty(PROCESS_ID, "policyQuoteTask");
+        userId = System.getProperty(USER_ID, "jboss");
 
         Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put(Driver.DRIVER_NAME, System.getProperty(Driver.DRIVER_NAME, "Azra"));
@@ -53,6 +59,7 @@ public class SimpleTaskClient extends AbstractJavaSamplerClient {
         StringBuilder sBuilder = new StringBuilder();
         sBuilder.append("\n\t"+DEPLOYMENT_ID+" : "+deploymentId);
         sBuilder.append("\n\t"+PROCESS_ID+" : "+processId);
+        sBuilder.append("\n\t"+USER_ID+" : "+userId);
         for (Map.Entry entry : parameters.entrySet()) {
             sBuilder.append("\n\t"+entry.getKey() + " : " + entry.getValue());
         }
@@ -60,22 +67,39 @@ public class SimpleTaskClient extends AbstractJavaSamplerClient {
 
         // 1)  start new process
         final long sendTime = System.currentTimeMillis();
-        long processInstanceId = 0L;
+        long pInstanceId = 0L;
         int ksessionId =0;
         Map<String, Object> returnMap = null;
         SampleResult results = new SampleResult();
         results.sampleStart();
         try {
-            returnMap = kSessionProxy.startProcessAndReturnId(processId, parameters, deploymentId);
-            if(returnMap == null){
-                results.setResponseMessage("pInstance creation failed for processId = "+processId);
-                results.setSuccessful(false);
-            }else {
-                processInstanceId = (Long)returnMap.get(IKnowledgeSession.PROCESS_INSTANCE_ID);
-                results.setResponseMessage("pInstanceId = "+processInstanceId);
-                results.setSuccessful(true);
-                results.setResponseCodeOK();
-            }
+            sBuilder = new StringBuilder();
+            returnMap = kProxy.startProcessAndReturnId(processId, parameters, deploymentId);
+            pInstanceId = (Long)returnMap.get(IKnowledgeSession.PROCESS_INSTANCE_ID);
+            sBuilder.append("pInstanceId = "+pInstanceId);
+
+            // sleep for two seconds to ensure all has been flushed to database prior to querying
+            Thread.sleep(2000);
+
+            // query for tasks by pInstanceId
+            List<TaskSummary> tasks = tProxy.getTasksAssignedAsPotentialOwner(userId, ITaskService.ENGLISH);
+            TaskSummary tObj = tasks.get(0);
+            sBuilder.append("taskId = "+tObj.getId());
+
+            // claim task
+            tProxy.claimTask(tObj.getId(), userId);
+
+            // start task
+            tProxy.startTask(tObj.getId(), userId);
+
+            // complete task
+            parameters = new HashMap<String, Object>();
+            parameters.put("taskPrice", new Integer(450));
+            tProxy.completeTask(tObj.getId(), parameters, userId);
+
+            results.setResponseMessage(sBuilder.toString());
+            results.setSuccessful(true);
+            results.setResponseCodeOK();
         } catch(javax.ejb.EJBTransactionRolledbackException x) {
             sBuilder = new StringBuilder();
             sBuilder.append("run() javax.ejb.EJBTransactionRolledbackException thrown when attempting to start process. cause = \n\t");
